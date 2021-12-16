38e08fa866d9c032cd61f9eea79fc521af79d971
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-16 23:36:56 +0300

    1612 2336

--- TelegramBot/sandbox.py
+++ TelegramBot/sandbox.py
@@ -6,7 +6,9 @@ bot = telebot.TeleBot(my_token)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+        bot.send_message(message.from_user.id, "Привет, как тебя зовут?")
+        answer = message.text
+        bot.send_message(message.from_user.id, "Очень приятно, " + str(answer))
     elif message.text == "Let's":
         bot.send_message(message.from_user.id, "GO!")
     elif message.text == "/help":
