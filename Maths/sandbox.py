79e09b42bceabc57e606090bda7172339f838a77
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-02-27 16:48:31 +0300

    2702 1648

--- Maths/sandbox.py
+++ Maths/sandbox.py
@@ -1,1 +1,12 @@
 from sympy import *
+import random
+
+x,f,g,h = symbols('x f g h')
+
+f = (x-1)*(x+2)*(x-4)
+g = (x-3)*(x+3)*(x-7)*(x+6)
+h = (x-2)*(x+1)*(x-3)*(x+3)*(x-4)
+
+print(f.expand())
+print(g.expand())
+print(h.expand())
