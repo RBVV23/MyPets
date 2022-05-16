0a7203f00aa8372a3a5774a0754cb0b842e1aa1d
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-05-16 16:55:10 +0300

    1605 1655

--- Maths/sandbox.py
+++ Maths/sandbox.py
@@ -4,20 +4,15 @@ import random
 
 x, f, g, h = symbols('x f g h')
 
-
 min = -10
 max = 10
 x_i = [random.randint(min, max)]
 N = 3
 f = x - x_i[0]
-for i in range(1,N):
+for i in range(1, N):
    x_i.append(random.randint(1, 10))
    f = f*(x-x_i[i])
-
 
 print(x_i)
 print(f.expand())
 print(f)
-
-
-
