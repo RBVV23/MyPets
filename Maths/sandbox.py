034ae43ac91033fe36872f5d464c866a4d07f43f
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-05-19 23:31:07 +0300

    1905 2331

--- Maths/sandbox.py
+++ Maths/sandbox.py
@@ -2,17 +2,17 @@ from sympy import *
 import random
 
 
-x, f, g, h = symbols('x f g h')
-
-min_x = -10
-max_x = 10
-x_i = [random.randint(min_x, max_x)]
-N = 3
-f = x - x_i[0]
-for i in range(1, N):
-   x_i.append(random.randint(1, 10))
-   f *= (x - x_i[i])
-
-print(x_i)
-print(f.expand())
-print(f)
+# x, f, g, h = symbols('x f g h')
+#
+# min_x = -10
+# max_x = 10
+# x_i = [random.randint(min_x, max_x)]
+# N = 3
+# f = x - x_i[0]
+# for i in range(1, N):
+#    x_i.append(random.randint(1, 10))
+#    f *= (x - x_i[i])
+#
+# print(x_i)
+# print(f.expand())
+# print(f)
