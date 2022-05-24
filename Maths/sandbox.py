9ad2307a17b7873c6420dae7f2d93e522a4772fa
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-05-24 20:02:21 +0300

    2405 2002

--- Maths/sandbox.py
+++ Maths/sandbox.py
@@ -15,6 +15,7 @@ repeat = True
 for i in range(N):
     if fraction_roots:
         chisl = random.randint(min_x, max_x)
+        print(chisl)
         znam = random.randint(2, max_x)
         root = Fraction(chisl, znam)
 
